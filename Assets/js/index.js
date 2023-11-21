847bf5cca4b5e526a2e9ce25ed5d9fdba9166146
joseph hancock <joe.hancock95@gmail.com>
2023-11-20 22:36:26 -0500

    minor changes

---
+++ Assets/js/index.js
@@ -0,0 +1,57 @@
+var elements = document.getElementsByClassName("column");
+
+// Declare a "loop" variable
+var i;
+
+// Full-width images
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "100%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "25%";
+  }
+}
+
+
+
+/*
+ * @name Setup and Draw
+ * @description The code inside the draw() function runs continuously from top
+ * to bottom until the program is stopped.
+ */
+let y = 100;
+
+// The statements in the setup() function
+// execute once when the program begins
+function setup() {
+  // createCanvas must be the first statement
+
+  
+  createCanvas(windowWidth, 200);
+  stroke(0); // Set line drawing color to white
+  frameRate(30);
+}
+// The statements in draw() are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
+// line is executed again.
+function draw() {
+  background(255); // Set the background to black
+  y = y - 1;
+  if (y < 0) {
+    y = height;
+  }
+  line(0, y, width, y);
+}
